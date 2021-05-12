df1a3cf2243562d7cf9829c94d2f5face79d7dd9
JelTur <lenagalaxygold@gmail.com>
2021-05-12 13:49:52 +0300

    create database and insrt data

---
+++ src/main/java/Warehouse/Main.java
@@ -0,0 +1,87 @@
+package Warehouse;
+
+import java.sql.*;
+import java.util.Scanner;
+
+import static Warehouse.SQL.*;
+
+
+public class Main {
+    // JDBC driver name and database URL
+    private static final String JDBC_DRIVER = "org.h2.Driver";
+    private static final String DB_URL = "jdbc:h2:./Warehouse";
+
+    //  Database credentials
+    private static final String USER = "sa";
+    private static final String PASS = "";
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = getConnection()) {
+            prepareDatabase(connection);
+            workWithConnection(connection);
+        } catch (SQLException throwables) {
+            System.out.println(throwables.getMessage());
+            throwables.printStackTrace();
+        }
+    }
+
+    private static void prepareDatabase(Connection connection) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(CREATE_TABLE_PRODUCT);
+            statement.executeUpdate(CREATE_TABLE_LOCATION);
+            statement.executeUpdate(INSERT_INTO_LOCATION);
+            statement.executeUpdate(INSERT_INTO_PRODUCT);
+        }
+    }
+
+    private static void workWithConnection(Connection connection) throws SQLException {
+        while (true) {
+            WarehouseAction nextAction = getActionFromUser();
+            switch (nextAction) {
+                case PRINT_ALL_PRODUCTS:
+                    break;
+                case PRINT_ALL_REFRIDGERATOR_PRODUCTS:
+                    break;
+                case PRINT_ALL_FREEZER_PRODUCTS:
+                    break;
+                case PRINT_ALL_SHELVES_PRODUCTS:
+                    break;
+                case ADD_PRODUCTS:
+                    break;
+                case REMOVE_PRODUCTS:
+                    break;
+                case PRINT_BY_EXPIRY_DATE:
+                    break;
+                case PRINT_ALL_PRODUCTS_WITH_EXPIRY_DATE_LESS_THAN_3_DAYS:
+                    break;
+                case ORDER_PRODUCTS:
+                    break;
+                case EXIT:
+                    return;
+            }
+        }
+    }
+
+    private static WarehouseAction getActionFromUser() {
+        while (true) {
+            System.out.println("Please select an action");
+            for (int i = 0; i < WarehouseAction.values().length; i++) {
+                System.out.println(i + "\t" + WarehouseAction.values()[i]);
+            }
+            // scanner.nextInt(); - would not read new line characters
+            int selected = Integer.parseInt(scanner.nextLine());
+            if (WarehouseAction.values().length <= selected || selected < 0) {
+                System.out.println("You entered incorrect value");
+                continue;
+            }
+            return WarehouseAction.values()[selected];
+        }
+    }
+}
+
---
+++ src/main/java/Warehouse/SQL.java
@@ -0,0 +1,65 @@
+package Warehouse;
+
+
+public class SQL {
+    public static final String INSERT_INTO_PRODUCT =
+            "INSERT INTO Product" +
+                    "(product,quantity,ExpDate,IDLocation) VALUES" +
+                    "('Sour_cream', 2, date '2021-05-31',1)," +
+                    "('Eggs', 10,date '2021-06-01',1)," +
+                    "('Zucchini', 3,date '2021-05-30',1)," +
+                    "('Chicken', 4,date '2021-05-31',2)," +
+                    "('Salad', 2, date '2021-06-01',1)," +
+                    "('Ice_cream', 5, date '2021-06-20',2)," +
+                    "('Pasta', 1, date '2021-06-05',3)," +
+                    "('Cucumber', 2,date '2021-06-01',1)," +
+                    "('Ice_cream', 5, date '2021-06-20',2)," +
+                    "('Onion', 7,date '2021-06-18',1)," +
+                    "('Bread', 2,date '2021-05-20',3);";
+
+    public static final String CREATE_TABLE_PRODUCT =
+            "CREATE TABLE IF NOT EXISTS Product (" +
+                    "id BIGINT auto_increment PRIMARY KEY," +
+                    "product VARCHAR(255) NOT NULL," +
+                    "quantity BIGINT NOT NULL," +
+                    "ExpDate date   NOT NULL," +
+                    "IDLocation BIGINT NOT NULL," +
+                    "foreign key (IDLocation) references Location(id)" +
+                    ");";
+
+    public static final String CREATE_TABLE_LOCATION =
+            "CREATE TABLE IF NOT EXISTS Location (" +
+                    "id BIGINT auto_increment PRIMARY KEY," +
+                    "Location1 VARCHAR(20) NOT NULL," +
+                    "Storagetemperature VARCHAR(20) NOT NULL" +
+                    ");";
+
+    public static final String INSERT_INTO_LOCATION =
+            "INSERT INTO Location" +
+                    "(Location1,Storagetemperature) VALUES" +
+                    "('Refrigerator','-18C')," +
+                    "('Freezer','+2 -+8C')," +
+                    "('Shelves','+20C');";
+
+    public static final String PRINT_ALL_PRODUCTS = "SELECT(Product) FROM Product;";
+    public static final String PRINT_ALL_REFRIDGERATOR_PRODUCTS = "SELECT (w.product,l.location1, w.IDLocation) FROM Product w\\n\" +\n" +
+            "            \"    JOIN Location l ON w.IDLocation = l.IDLocation;";
+    public static final String PRINT_ALL_FREEZER_PRODUCTS = "SELECT (w.product,l.location1, w.IDLocation) FROM Product w\n" +
+            "    JOIN Location l ON w.IDLocation = l.IDLocation;";
+    public static final String PRINT_ALL_SHELVES_PRODUCTS = "SELECT (w.product,l.location1, w.IDLocation) FROM Product w\\n\" +\n" +
+            "            \"    JOIN Location l ON w.IDLocation = l.IDLocation;";
+    public static final String PRINT_BY_EXPIRY_DATE = "SELECT * FROM Product\n" +
+            "    WHERE ExpDate < NOW()\n" +
+            "    ORDER BY ExpDate ASC;";
+    public static final String ADD_PRODUCTS = "INSERT INTO Product" +
+            " (product,quantity,ExpDate,IDLocation) VALUES" +
+            "(?, ?, ?, ?);";
+    public static final String REMOVE_PRODUCTS = "DELETE FROM Product WHERE id = ?;";
+    public static final String PRINT_ALL_PRODUCTS_WITH_EXPIRY_DATE_LESS_THAN_3_DAYS = "SELECT *\n" +
+            "    FROM  Product\n" +
+            "    WHERE  ExpDate <  TRUNC( SYSDATE ) + INTERVAL '3' DAY   \n" +
+            "    AND    ExpDate >= SYSDATE;";
+    public static final String ORDER_PRODUCTS = "SELECT * FROM Product \n" +
+            "    WHERE quantity\n" +
+            "    ORDER BY quantity ASC;";
+}
